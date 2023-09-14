46534c764f486fddfa0665b1f1b8c99aaf82c25c
Brent Buchholz <brentbuchholz@gmail.com>
2023-09-13 21:23:13 -0500

    Add getNoteName

--- src/pages/myrKiSs/musicTools.js
+++ src/pages/myrKiSs/musicTools.js
@@ -1,3 +1,57 @@
+export function getNoteName(noteValue) {
+  let name = 'NOTENAME'
+
+  let coaxedValue = noteValue
+
+  while (coaxedValue < 0)
+    coaxedValue += 12
+
+  while (coaxedValue > 11)
+    coaxedValue -= 12
+
+  switch (coaxedValue) {
+    case 0:
+      name = 'C'
+      break
+    case 1:
+      name = 'C#/Db'
+      break
+    case 2:
+      name = 'D'
+      break
+    case 3:
+      name = 'D#/Eb'
+      break
+    case 4:
+      name = 'E'
+      break
+    case 5:
+      name = 'F'
+      break
+    case 6:
+      name = 'F#/Gb'
+      break
+    case 7:
+      name = 'G'
+      break
+    case 8:
+      name = 'G#/Ab'
+      break
+    case 9:
+      name = 'A'
+      break
+    case 10:
+      name = 'A#/Bb'
+      break
+    case 11:
+      name = 'B'
+      break
+    default:
+      name = '?'
+  }
+
+  return name
+}
 export function getDiminishedChord(rootNote) {
   return `${rootNote} Diminished`
 }
@@ -5,7 +59,17 @@ export function getAugmentedChord(rootNote) {
   return `${rootNote} Augmented`
 }
 export function getMajorChord(rootNote) {
-  return `${rootNote} Major`
+  const pattern = [0, 4, 7]
+  const chord = []
+
+  for (const patternStep of pattern)
+    chord.push(getNoteName(patternStep + rootNote))
+
+  let output = `${getNoteName(rootNote)} Major -> `
+
+  output += ''
+
+  return output
 }
 export function getMinorChord(rootNote) {
   return `${rootNote} Minor`
--- test/musicTools.test.js
+++ test/musicTools.test.js
@@ -1,6 +1,30 @@
 // sum.test.js
-import { expect, test } from 'vitest'
-import { getMajorChord } from '../src/pages/myrKiSs/musicTools'
+import {
+  expect,
+  test,
+} from 'vitest'
+
+import {
+  getMajorChord,
+  getNoteName,
+} from '../src/pages/myrKiSs/musicTools'
+
+test('gets note name for various notes', () => {
+  expect(getNoteName(-1)).toBe('B')
+  expect(getNoteName(0)).toBe('C')
+  expect(getNoteName(1)).toBe('C#/Db')
+  expect(getNoteName(2)).toBe('D')
+  expect(getNoteName(3)).toBe('D#/Eb')
+  expect(getNoteName(4)).toBe('E')
+  expect(getNoteName(5)).toBe('F')
+  expect(getNoteName(6)).toBe('F#/Gb')
+  expect(getNoteName(7)).toBe('G')
+  expect(getNoteName(8)).toBe('G#/Ab')
+  expect(getNoteName(9)).toBe('A')
+  expect(getNoteName(10)).toBe('A#/Bb')
+  expect(getNoteName(11)).toBe('B')
+  expect(getNoteName(12)).toBe('C')
+})
 
 test('adds 1 + 2 to equal 3', () => {
   expect(getMajorChord(0)).toBe('C Major -> C, E, G')
