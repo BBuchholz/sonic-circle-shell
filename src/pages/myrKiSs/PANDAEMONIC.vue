d2e96e4944e685d38425dd3b16d6150a201f7fe4
Brent Buchholz <brentbuchholz@gmail.com>
2023-09-22 08:55:28 -0500

    Add PDMNC

---
+++ src/pages/myrKiSs/PANDAEMONIC.vue
@@ -0,0 +1,191 @@
+<template>
+  <div>
+    <div class="display">
+      <h1>
+        <a
+          href="https://music.youtube.com/playlist?list=OLAK5uy_lk5yKF8s-b_9QYEq-qpi1JYQrf5x4s9GQ&si=n7o0LRN1txE7y6-Y"
+          target="_blank"
+        >
+          The Early Years: A Pesitlential Inauguration Of darkRhyme Prosody
+        </a>
+      </h1>
+      <ul>
+        <li>
+          <a
+            href="https://music.youtube.com/watch?v=dz2KntloLA4&si=-d5xM8YvIRidGeaD"
+            target="_blank"
+          >
+            Avenging Angel
+          </a>
+          <a
+            href="https://genius.com/Pandaemonic-avenging-angel-lyrics"
+            target="_blank"
+          >
+            (lyrics)
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://music.youtube.com/watch?v=Alf0d8kTshM&si=VtoIqiFrfMm7u1tK"
+            target="_blank"
+          >
+            Friend to the Dead
+          </a>
+          <a
+            href="https://genius.com/Pandaemonic-friend-to-the-dead-lyrics"
+            target="_blank"
+          >
+            (lyrics)
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://music.youtube.com/watch?v=h4ownJGzzsY&si=pbh5gAobj966cycV"
+            target="_blank"
+          >
+            TechGnostic
+          </a>
+          <a
+            href="https://genius.com/Pandaemonic-techgnostic-lyrics"
+            target="_blank"
+          >
+            (lyrics)
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://music.youtube.com/watch?v=F1r4qMMu7nU&si=9wbDjmVtg92KubZM"
+            target="_blank"
+          >
+            Resplendent Descent
+          </a>
+          <a
+            href="https://genius.com/Pandaemonic-resplendent-descent-lyrics"
+            target="_blank"
+          >
+            (lyrics)
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://music.youtube.com/watch?v=3VTgsnKlBro&si=t-XbikCpWOpoaBRd"
+            target="_blank"
+          >
+            2001
+          </a>
+          <a
+            href="https://genius.com/Pandaemonic-2001-lyrics"
+            target="_blank"
+          >
+            (lyrics)
+          </a>
+        </li>
+      </ul>
+    </div>
+    <div class="display">
+      <h1>
+        <a
+          href="https://music.youtube.com/playlist?list=OLAK5uy_lAam8lqO9G6lrQbBV1j5HkKIUwJo4t8rY&si=JZJqDX4TyCXKUmbo"
+          target="_blank"
+        >
+          The DarkRhyme Chronicles, Vol. 2
+        </a>
+      </h1>
+      <ul>
+        <li>
+          <a
+            href="https://music.youtube.com/watch?v=w4hmK9r0EHw&si=cHtf3sKMd9H_XTWy"
+            target="_blank"
+          >
+            I.S.G. (Interstitial Sub Genre)
+          </a>
+          <a
+            href="https://genius.com/Pandaemonic-isg-interstitial-sub-genre-lyrics"
+            target="_blank"
+          >
+            (lyrics)
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://music.youtube.com/watch?v=n445z-_nPyI&si=sXTisaQC-0ztL9Gv"
+            target="_blank"
+          >
+            Arcadian
+          </a>
+          <a
+            href="https://genius.com/Pandaemonic-arcadian-lyrics"
+            target="_blank"
+          >
+            (lyrics)
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://music.youtube.com/watch?v=umbQBNbEWLo&si=eXVxHNA2mdK2mdJ5"
+            target="_blank"
+          >
+            Backwards Blessing
+          </a>
+          <a
+            href="https://genius.com/Pandaemonic-backwards-blessing-lyrics"
+            target="_blank"
+          >
+            (lyrics)
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://music.youtube.com/watch?v=Elv5HVhXoHM&si=ciDW7oyIcUJIEMcY"
+            target="_blank"
+          >
+            Heathen
+          </a>
+          <a
+            href="https://genius.com/Pandaemonic-heathen-lyrics"
+            target="_blank"
+          >
+            (lyrics)
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://music.youtube.com/watch?v=jgHuK7XQ7xA&si=aFibDm-enHmgYvLn"
+            target="_blank"
+          >
+            Maatian
+          </a>
+          <a
+            href="https://genius.com/Pandaemonic-maatian-lyrics"
+            target="_blank"
+          >
+            (lyrics)
+          </a>
+        </li>
+      </ul>
+    </div>
+    <!-- <a href="https://sonic-circle.netlify.app/" target="_blank"> -->
+    <img class="card" src="https://madamadam.s3.us-east-2.amazonaws.com/PANDAEMONIC-02ea_CARD.png">
+    <!-- </a> -->
+  </div>
+</template>
+
+<style>
+  .display {
+    margin: 40px;
+  }
+
+  .card {
+    max-width: 50%;
+    height: auto;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 50px;
+    margin-bottom: 50px;
+    border: 5px;
+    border-color: white;
+    border-style: solid;
+    border-radius: 1%;
+  }
+</style>
--- src/pages/myrKiSs/PLAY.vue
+++ src/pages/myrKiSs/PLAY.vue
@@ -1,25 +1,50 @@
 <template>
   <div>
-    <p>HERE WE NEED VARIOUS ACTIVITIES, BUILD CARDS FOR EACH, SELECRT FROM ALL AVAILABLE HERE</p>
-    <p>TRY TO RANDOMIZE THEM ON LOAD?</p>
-    <!-- <a href="https://anxious-grove.netlify.app/" target="_blank"> -->
-    <img class="card" src="/PLAY-4249.png">
-    <!-- </a> -->
+    <div class="display">
+      <p>AppZ We Like</p>
+      <ul>
+        <li>COMING SOON (these are just placeholders for design)</li>
+        <li>BandLab</li>
+        <li>Music Maker Jam</li>
+      </ul>
+    </div>
+    <div class="display">
+      <p>Gear We Use</p>
+      <ul>
+        <li>COMING SOON (these are just placeholders for design)</li>
+        <li>audio interface specs</li>
+        <li>mic specs (both)</li>
+        <li>keyboard</li>
+        <li>effects pedal</li>
+        <li>drums</li>
+      </ul>
+    </div>
+
+    <p class="display">
+      CLICK THE PLAY CARD TO SUBMIT PROJECKT LINKS
+    </p>
+    <a href="https://forms.gle/WAJJntcmGdxcdHk48" target="_blank">
+      <img class="card" src="/PLAY-4249.png">
+    </a>
   </div>
 </template>
 
 <style>
-    .card {
-        max-width: 50%;
-        height: auto;
-        display: block;
-        margin-left: auto;
-        margin-right: auto;
-        margin-top: 50px;
-        margin-bottom: 50px;
-        border: 5px;
-        border-color: white;
-        border-style: solid;
-        border-radius: 1%;
-    }
+  .display {
+    margin: 40px;
+  }
+
+  .card {
+      max-width: 50%;
+      height: auto;
+      display: block;
+      margin-left: auto;
+      margin-right: auto;
+      margin-top: 50px;
+      margin-bottom: 50px;
+      border: 5px;
+      border-color: white;
+      border-style: solid;
+      border-radius: 1%;
+  }
 </style>
